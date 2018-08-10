7dbb8ba706e2edca0c23f1800f81f741ca6d3534
Matthew Herron <matthew.herron77@gmail.com>
2018-08-09 21:20:47 -0500

    Additional merge conflict fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< Updated upstream
-import './index.css';
-import App from './App';
-=======
 import App from './components/app';
->>>>>>> Stashed changes
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
