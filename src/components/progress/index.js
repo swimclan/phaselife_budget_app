b1f1bc60bd007c1d3fccc9f6a22847f041283ad4
Matthew Herron <matthew.herron77@gmail.com>
2018-08-13 21:03:22 -0500

    Spent amount on progress bar

--- src/components/progress/index.js
+++ src/components/progress/index.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Progress extends Component {
+  constructor(props) {
+    super(props);
+    this.progressRef = React.createRef();
+    this.state = {
+      displayed: true
+    }
+  }
+
+  componentDidMount() {
+    this.progressRef.current.addEventListener('transitionstart', (e) => {
+      this.setState({ displayed: false });
+    });
+    this.progressRef.current.addEventListener('transitionend', (e) => {
+      this.setState({ displayed: true });
+    });
+  }
+
   render() {
     const percent = this.props.current / this.props.limit * 100;
     let progressBarClass = 'bta-progress-bar';
@@ -10,7 +27,13 @@ export default class Progress extends Component {
     percent > 66 && (progressBarClass += ' danger');
     return (
       <div className="bta-progress-container">
-        <div className={progressBarClass} style={ { width: `${percent}%` } }></div>
+        <div 
+          ref={this.progressRef}
+          className={progressBarClass}
+          style={ { width: `${percent > 100 ? 100 : percent}%`, paddingRight: percent > 10 ? '5px' : '0' } }
+        >
+          <span className={'bta-progress-spent' + (this.state.displayed ? ' displayed' : '')}>{percent > 10 ? `$${this.props.current}`: null}</span>
+        </div>
       </div>
     );
   }
