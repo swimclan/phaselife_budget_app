1f5ed3f5f77aa81048cd011861cc1e904988f030
Matthew Herron <matthew.herron77@gmail.com>
2018-08-15 19:40:44 -0500

    Paddles for navigating through categories

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 export default class Categories extends Component {
   constructor(props) {
     super(props);
-    this.onClickCategory = this.onClickCategory.bind(this); 
+    this.onClickCategory = this.onClickCategory.bind(this);
+    this.move = this.move.bind(this);
     this.state = {
       index: 0
     }
@@ -16,17 +17,29 @@ export default class Categories extends Component {
 
   onClickCategory(e) {
     e.stopPropagation();
-    let index = (this.state.index + 1) % this.props.categories.length
+    let index = this.calcNextIndex(1);
     this.setState({ index });
     this.props.onChange(this.props.categories[index]);
+  }
+
+  move(steps) {
+    let index = this.calcNextIndex(steps);
+    this.setState({ index });
+  }
+
+  calcNextIndex(step) {
+    const currentIndex = this.state.index || this.props.categories.length;
+    return (currentIndex + step) % this.props.categories.length;
   }
 
   render() {
     return (
       <div className="bta-categories-container">
+        <button className="bta-categories-paddle paddle-left" onClick={() => this.move(-1)}></button>
         <section onClick={this.onClickCategory} className="bta-categories-select-container">
           <article className="bta-categories-option">{this.props.categories[this.state.index].name}</article>
         </section>
+      <button className="bta-categories-paddle paddle-right" onClick={() => this.move(1)}></button>
       </div>
     );
   }
